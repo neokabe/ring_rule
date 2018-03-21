c8d8a271fd01251fb657c7ea8d26633cfd7fd92e
neokabe <34715190+neokabe@users.noreply.github.com>
2018-03-21 18:48:39 +0000

    sepperated javascript into standalne files

---
+++ J_script/reg_events.js
@@ -0,0 +1,39 @@
+
+
+//java script events//
+// java script event for revealing the register page input fields  //
+
+
+var press1 = document.getElementById("field_btn");
+var sec = document.getElementById("last");
+var = press2 = document.getElementByType("submit")
+
+
+
+
+li.list{
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.ready{
+	.submit(){
+		//api...//
+		alert("are you sure you whish to submit this data?");
+	}
+
+}
