cfffbfeda7fcf367bdfe855d1331e1d9b28dc6c0
neokabe <34715190+neokabe@users.noreply.github.com>
2018-03-21 19:15:04 +0000

    reverted back to old reg ids added reveal function in js page

--- J_script/reg_events.js
+++ J_script/reg_events.js
@@ -7,15 +7,24 @@
 var press1 = document.getElementById("field_btn");
 var sec = document.getElementById("last");
 var = press2 = document.getElementByType("submit")
+var = checkFields = document.getElementById("field5");
 
 
 
+// document.ready li.list(){
 
-document.ready li.list(){
 
+// }
 
-}
+ function reveal(){
+ 	field5.input(!=="");
+ 	alert("done");
+ 	sec.show(500);
+ }
 
+press2.addEventListener("input", reveal )
+  function reveal()
+)
 
 
 
