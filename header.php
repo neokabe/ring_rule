d00ef9706a14f902121cd7241261ac283c78fe05
neokabe <34715190+neokabe@users.noreply.github.com>
2018-04-17 15:25:58 +0100

    changed navbar link from home to index.php

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 						        <span class="icon-bar"></span>
 						        <span class="icon-bar"></span>
 						      </button>
-						      <a class="navbar-brand" href="Ghome.html">Creatorio</a>
+						      <a class="navbar-brand" href="index.php">Creatorio</a>
 						    </div>
 
 						    <!-- Collect the nav links, forms, and other content for toggling -->
