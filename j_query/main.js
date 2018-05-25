de38d6db2139d0824d726253745156926086d150
neokabe <34715190+neokabe@users.noreply.github.com>
2018-05-25 14:44:29 +0100

    wrote script for prev button nd centred slide banner

--- j_query/main.js
+++ j_query/main.js
@@ -1,6 +1,6 @@
 //slide show script
 $(document).ready(function(){
-	$('.next').on('click',function(){
+	$('.next').on('click', function(){
 
 		var currentImg = $('.active');
 		var nextImg = currentImg.next();
@@ -8,6 +8,18 @@ $(document).ready(function(){
 		if(nextImg.length){
 			currentImg.removeClass('active').css('z-index', -10);
 			nextImg.addClass('active').css('z-index', 10);
-		}
+		} 
+	});
+
+	$('.prev').on('click', function(){
+
+		var currentImg = $('.active');
+		var prevImg = currentImg.prev();
+
+		if(prevImg.length){
+			currentImg.removeClass('active').css('z-index', -10);
+			prevImg.addClass('active').css('z-index', 10);
+		} 
+	});
 
 });
