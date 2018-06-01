d8d3e8ffc798584b00e7e2b16dc53090804a32a4
neokabe <34715190+neokabe@users.noreply.github.com>
2018-06-01 17:48:31 +0100

    removed all tile from nav bar

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 						    <!-- Collect the nav links, forms, and other content for toggling -->
 						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						      <ul class="nav navbar-nav">
-						        <li class="active"><a href="#">All <span class="sr-only">(current)</span></a></li>
+						        <!-- <li class="active"><a href="#">All <span class="sr-only">(current)</span></a></li> -->
 						        <li><a href="Register.php"> Register</a></li>
 								<li><a href="Logon.php">Sign in</a></li>
 								<li><a href="about.php">About</a></li>
