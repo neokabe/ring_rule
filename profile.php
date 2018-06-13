21e8ff35b17f97da47502438e4ca14f483b1b16d
neokabe <34715190+neokabe@users.noreply.github.com>
2018-06-13 15:47:50 +0100

    created user profile page

---
+++ profile.php
@@ -0,0 +1,38 @@
+<?php
+
+	include 'header.php' ;
+
+?>
+
+
+<div class= "left-panel">
+	<h3>upload my profile image  </h3>
+	<form action="uploaud.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="file">
+		<button type="submit" name="submitpro" ></button>
+		
+
+	</form>
+
+</div>
+
+
+<container></container>
+	
+<div class="right-panel">
+	<h4>settings</h4>
+</div>
+
+
+
+
+
+
+
+
+
+<?php
+include 'footer.php';
+
+
+?>
