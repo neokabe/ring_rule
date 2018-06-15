b26bd7343ec163188343255711f6cc4c0d6936f2
neokabe <34715190+neokabe@users.noreply.github.com>
2018-06-15 14:39:13 +0100

    added working upload script and linked it

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@
 
 <div class= "left-panel">
 	<h3>upload my profile image  </h3>
-	<form action="uploads.php" method="POST" enctype="multipart/form-data">
+	<form action="uploads2.php" method="POST" enctype="multipart/form-data">
 		<input type="file" name="file">
 		<button type="submit" name="submitpro" class="btn btn-success" >Upload</button>
 		
