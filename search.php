bbb01fdca9b1507139f74386c01cd9bb155d3537
neokabe <34715190+neokabe@users.noreply.github.com>
2018-07-11 14:33:31 +0100

    added advance search feature for individual words

--- search.php
+++ search.php
@@ -16,13 +16,10 @@ if (isset($_POST['submit-search'])) {
  $Search = $_POST('Search');
 	//check if search field is empty 
 
-	if (empty($Search)) {
+	if (empty($search)) {
 		exit();
 	} else {
-		// check for gaps 
-		if (condition) {
-			# code...
-		} else{
+		
 			// check if title is keyword conained in search string
 			 $sql = "SELECT * FROM article WHERE a_title LIKE ? OR a_text LIKE ? OR a_author LIKE ? OR a_date LIKE ?;";
 
@@ -36,9 +33,62 @@ if (isset($_POST['submit-search'])) {
 				header("Location: ../header.php?search=error");
 	 	 	exit();
 			} else{
+			mysqli_stmt_bind_param($stmt, "ssss", $binditem, $binditem, $binditem, $binditem);
 
+	 	 	mysqli_stmt_execute($stmt);
+
+
+	 	 $result = mysqli_stmt_get_result($stmt);
+	 	 $check =mysqli_num_rows($result);
+
+	 	 	echo "There are ".$check." results";
+
+
+	 	 	if ($check > 0 ) {
+	 	 	while ($row =mysqli_fetch_assoc($result)) {
+			  echo "<a href='article.php?title=".$row['a_title']."&date=".$row['a_date']."'><div class= 'article-box'>
+			  	<h3>".$row['a_title']."</h3>
+			  	<p>".$row['a_text']."</p>
+			  	<p>".$row['a_date']."</p>
+			  	<p>".$row['a_author']."</p>
+			  	</div></a>";
+			  	}
+	 	 	} else {
+				// check for gaps/ individual words
+				$searchbit = explode(" ", $search);
+
+				foreach($searchbit as $value) {
+					$bindbit= $searchbit($value);
+
+				$sql = "SELECT * FROM article WHERE a_title LIKE ? OR a_text LIKE ? OR a_author LIKE ? OR a_date LIKE ?;";
+				mysqli_stmt_bind_param($stmt, "ssss", $bindbit, $bindbit, $bindbit,$bindbit);
+					mysqli_stmt_execute($stmt);
+
+					 $result = mysqli_stmt_get_result($stmt);
+					 $check2 = mysqli_num_rows($result);
+				if ($check2 > 0 ) {
+	 	 			while ($row =mysqli_fetch_assoc($result)) {
+			 echo "<a href='article.php?title=".$row['a_title']."&date=".$row['a_date']."'><div class= 'article-box'>
+			  	<h3>".$row['a_title']."</h3>
+			  	<p>".$row['a_text']."</p>
+			  	<p>".$row['a_date']."</p>
+			  	<p>".$row['a_author']."</p>
+			  	</div></a>";
+			  				}
+	 	 		} else {
+	 	 		echo "<p>There are no matching results</p>";
+	 	 							}
+
+				}
+
+				 
+	 	 		
+	 			
+	 	 	}
+
+				
 			}
-		}
+		
 		
 	}
 
