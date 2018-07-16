07be14e098097c2d87ad59504646ec69bc3a6f1b
neokabe <34715190+neokabe@users.noreply.github.com>
2018-07-16 13:49:02 +0100

    created email script for contact page

--- contact_us.php
+++ contact_us.php
@@ -5,18 +5,25 @@
 
 		
 
-		For use enquiries contact us at digiportfolio@thuneer.com
+		
 
 
 <main>
-<p>SEND email </p	>
+<p>SEND email </p>
 
-<form class="contact-form" action="contactform.php" method="post">
+<form class="contact-form" action="includes/contactform.php" method="post">
+
 	<input type="text" name="name" placeholder="Full name">
+	<input type="text" name="mail" placeholder="your email">
+	<input type="text" name="subject" placeholder="Subject">
+	<textarea name="message" placeholder="message"> </textarea>
+	<button type="submit" name="submit">SEND MAIL</button>
 	
 
 </form>	
 </main>
+
+For use enquiries contact us at digiportfolio@thuneer.com
 
 <?php
 
