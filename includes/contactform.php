3448c7c3d5717aa6ec2e8b3cda719a5358423ee8
neokabe <34715190+neokabe@users.noreply.github.com>
2018-07-16 14:08:33 +0100

    added documentationfor contact from script and completed email message format

--- includes/contactform.php
+++ includes/contactform.php
@@ -5,5 +5,18 @@ if (isset($_POST['submit'])) {
 	$subject =$_POST['subject'];
 	$mailFrom =$_POST['mail'];
 	$message =$_POST['message'];
+
+
+//doest work with gmail addresses(cng.nnadi@gmail.com)
+	//get a hosting service with a domain email inorder to forward messages to gmail addresses
+
+	$mailTo ="nnnn.mat@hotmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt= "You have recieved an email from ".$name.".\n\n".$message;  ; 
+
+
+	mail($mailTo, $subject, $txt, $headers);
+
+	header("Location: index.php?mailsent");
 	
 }
